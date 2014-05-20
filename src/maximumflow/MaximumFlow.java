659eefb084cfc7178e268a2347340df9e26fc984
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-19 19:57:10 -0500

    Maximum flow class file

---
+++ src/maximumflow/MaximumFlow.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package maximumflow;
+
+/**
+ * @author Dany
+ *
+ */
+public class MaximumFlow {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
